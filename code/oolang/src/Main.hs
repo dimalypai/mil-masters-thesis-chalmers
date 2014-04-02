1072a700ffdecd7b6b4be75e7fbc35958ccaa869
Dmytro Lypai <dmytrolypai@gmail.com>
2014-04-02 14:01:44 +0200

    OOLang. Very primitive user interface for oolc.
    Add options: interactive, help, dump-ast (not implemented).
    Add non-interactive mode (reading a file and outputting the AST).
    Very primitive, no error handling.

--- code/oolang/src/Main.hs
+++ code/oolang/src/Main.hs
@@ -1,7 +1,12 @@
 module Main where
 
 import System.IO
+import System.Environment
+import System.Console.GetOpt
+import System.FilePath
+import System.Exit
 import Text.Show.Pretty
+import Control.Monad (when)
 
 import OOLang.Lexer
 import OOLang.Parser
@@ -9,16 +14,48 @@ import OOLang.Parser
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
-    putStr "ooli> "
-    input <- getLine
-    if input == ":q"
-      then return ()
-      else do let p = lexer input |>
-                      parse "ooli"
-              case p of
-                Left err -> putStrLn $ prPrint err
-                Right pr -> putStrLn $ ppShow pr
+    (flags, nonOpts) <- getArgs >>= compilerOpts
+    when (Help `elem` flags) $ do
+      printHelp
+      exitSuccess
+    if Interactive `elem` flags
+      then do putStr "ooli> "
+              input <- getLine
+              if input == ":q"
+                then exitSuccess
+                else do let p = lexer input |>
+                                parse "ooli"
+                        case p of
+                          Left err -> putStrLn (prPrint err)
+                          Right pr -> putStrLn (ppShow pr)
               main
+      else do let filePath = head nonOpts
+              src <- readFile filePath
+              let p = lexer src |>
+                      parse (takeFileName filePath)
+              case p of
+                Left err -> putStrLn (prPrint err) >> exitFailure
+                Right pr -> putStrLn (ppShow pr) >> exitSuccess
+
+data Flag = Interactive | DumpAst | Help
+  deriving Eq
+
+options :: [OptDescr Flag]
+options =
+  [ Option ['i'] ["interactive"] (NoArg Interactive) "Interactive mode (REPL)"
+  , Option []    ["dump-ast"]    (NoArg DumpAst)     "Write AST to a file"  -- TODO
+  , Option ['h'] ["help"]        (NoArg Help)        "Prints this help information"
+  ]
+
+compilerOpts :: [String] -> IO ([Flag], [String])
+compilerOpts argv =
+  case getOpt Permute options argv of
+    (opts, nonOpts, []) -> return (opts, nonOpts)
+    (_, _, errs) -> ioError (userError (concat errs ++ "Try the -h (--help) option"))
+
+printHelp :: IO ()
+printHelp = putStr $ usageInfo header options
+  where header = "Usage: oolc [OPTIONS] file"
 
 (|>) :: a -> (a -> b) -> b
 a |> f = f a
