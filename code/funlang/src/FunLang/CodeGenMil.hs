81b6b18b009de1f42fe875693b3eb017bcbce1d1
Dmytro Lypai <dmytrolypai@gmail.com>
2014-05-02 12:47:18 +0200

    FunLang. Code generator. Add expression and monads. Not tested yet.

--- code/funlang/src/FunLang/CodeGenMil.hs
+++ code/funlang/src/FunLang/CodeGenMil.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- | Module responsible for MIL code generation.
 module FunLang.CodeGenMil
@@ -37,23 +37,53 @@ codeGenFunDef :: TyFunDef -> MIL.FunDef
 codeGenFunDef (FunDef _ srcFunName funSrcType tyFunEqs) =
   let funName = getFunName srcFunName
       milFunType = srcTypeToMilType funSrcType
-      milFunBody = codeGenFunEqs tyFunEqs
+      milFunBody = codeGenFunEqs tyFunEqs milFunType
   in MIL.FunDef (funNameMil funName) milFunType milFunBody
 
--- | TODO
-codeGenFunEqs :: [TyFunEq] -> MIL.Expr
-codeGenFunEqs tyFunEqs =
-  let funEqExprs = map (codeGenExpr . getFunEqBody) tyFunEqs
+-- | Takes function equations of the function definition and the transformed
+-- function type and returns an MIL expression.
+-- Type is needed for annotating monadic operations.
+codeGenFunEqs :: [TyFunEq] -> MIL.Type -> MIL.Expr
+codeGenFunEqs tyFunEqs milFunType =
+  let funEqExprs = map (codeGenExpr milFunType . getFunEqBody) tyFunEqs
   in head funEqExprs  -- TODO
 
-codeGenExpr :: TyExpr -> MIL.Expr
-codeGenExpr (LitE srcLit) = MIL.LitE (literalMil $ getLiteral srcLit)
-codeGenExpr (VarE _ varTy) = MIL.VarE $ MIL.VarBinder (var, milVarType)
-  where var = varMil $ getVarTyVar varTy
-        milVarType = typeMil $ getVarTyType varTy
--- TODO
-codeGenExpr (DoE _ [ReturnS _ tyExpr]) =
-  MIL.ReturnE (MIL.MTyMonad MIL.IO) (codeGenExpr tyExpr)
+-- | Expression code generation.
+-- Takes a transformed type of the containing function for annotating monadic
+-- operations.
+codeGenExpr :: MIL.Type -> TyExpr -> MIL.Expr
+codeGenExpr milFunType srcExpr =
+  case srcExpr of
+    LitE srcLit -> MIL.LitE (literalMil $ getLiteral srcLit)
+
+    VarE _ varTy ->
+      let var = varMil $ getVarTyVar varTy
+          milVarType = typeMil $ getVarTyType varTy
+      in MIL.VarE $ MIL.VarBinder (var, milVarType)
+
+    LambdaE _ varBinders tyBodyExpr ->
+      foldr (\vb mexpr ->
+          MIL.LambdaE (MIL.VarBinder ( varMil (getVar $ getBinderVar vb)
+                                     , srcTypeToMilType $ getBinderType vb)) mexpr)
+        (codeGenExpr milFunType tyBodyExpr)
+        varBinders
+
+    TypeLambdaE _ srcTypeVars tyBodyExpr ->
+      foldr (\tv mexpr ->
+          MIL.TypeLambdaE (typeVarMil tv) mexpr)
+        (codeGenExpr milFunType tyBodyExpr)
+        (map getTypeVar srcTypeVars)
+
+    TypeAppE _ tyAppExpr srcArgType ->
+      MIL.TypeAppE (codeGenExpr milFunType tyAppExpr) (srcTypeToMilType srcArgType)
+
+    ConNameE srcConName -> MIL.ConNameE $ conNameMil (getConName srcConName)
+
+    BinOpE _ srcBinOp tyExpr1 tyExpr2 -> codeGenBinOp (getBinOp srcBinOp) tyExpr1 tyExpr2 milFunType
+
+    ParenE _ tySubExpr -> codeGenExpr milFunType tySubExpr
+
+    DoE _ tyStmts -> codeGenDoBlock tyStmts milFunType
 
 literalMil :: Literal -> MIL.Literal
 literalMil UnitLit        = MIL.UnitLit
@@ -61,29 +91,61 @@ literalMil (IntLit i)     = MIL.IntLit i
 literalMil (FloatLit f _) = MIL.FloatLit f
 literalMil (StringLit s)  = MIL.StringLit s
 
+codeGenBinOp :: BinOp -> TyExpr -> TyExpr -> MIL.Type -> MIL.Expr
+codeGenBinOp App tyExpr1 tyExpr2 milFunType =
+  MIL.AppE (codeGenExpr milFunType tyExpr1) (codeGenExpr milFunType tyExpr2)
+
+-- | TODO
+codeGenDoBlock :: [TyStmt] -> MIL.Type -> MIL.Expr
+codeGenDoBlock [ReturnS _ tyExpr] milFunType@(MIL.TyApp (MIL.TyMonad monadType) _) =
+  MIL.ReturnE monadType (codeGenExpr milFunType tyExpr)
+
+-- | Internal type representation transformation.
+-- The most interesting is the 'TyApp' transformation, because it deals with
+-- monads. Type checker ensures that all type constructors are fully applied,
+-- so it is safe to construct monadic types in this way (by direct pattern
+-- matching), otherwise - we throw a panic.
 typeMil :: Type -> MIL.Type
 typeMil (TyVar typeVar) = MIL.TyVar $ typeVarMil typeVar
 typeMil (TyArrow t1 t2) = MIL.TyArrow (typeMil t1) (typeMil t2)
 typeMil (TyApp typeName typeArgs) =
-  foldl' (\mt t -> MIL.TyApp mt (typeMil t)) (MIL.TyTypeCon $ typeNameMil typeName) typeArgs
+  case (typeName, typeArgs) of
+    (TypeName "IO", [ioResultType]) ->
+      MIL.TyApp (MIL.TyMonad $ MIL.MTyMonad MIL.IO) (typeMil ioResultType)
+    (TypeName "IO", _) -> error "IO type is ill-formed"
+
+    (TypeName "State", [stateType, stateResultType]) ->
+      MIL.TyApp (MIL.TyMonad $ MIL.MTyMonad (MIL.State $ typeMil stateType)) (typeMil stateResultType)
+    (TypeName "State", _) -> error "State type is ill-formed"
+
+    _ -> foldl' (\mt t -> MIL.TyApp mt (typeMil t))
+           (MIL.TyTypeCon $ typeNameMil typeName)
+           typeArgs
 typeMil (TyForAll typeVar t) = MIL.TyForAll (typeVarMil typeVar) (typeMil t)
 
--- | TODO
-monadMil :: SrcType -> MIL.TypeM
-monadMil (SrcTyCon srcTypeName) = MIL.MTyMonad MIL.IO  -- TODO
-
+-- | Source type representation transformation.
+-- Monadic types are transformed in different cases depending on their kind.
+-- * IO has kind `* => *` so it is caught in 'SrcTyCon'
+-- * State has kind `* => * => *` so it must be caught earlier, in 'SrcTyApp',
+-- in order to construct an MilMonad with the type of the state.
 srcTypeToMilType :: SrcType -> MIL.Type
 srcTypeToMilType (SrcTyCon srcTypeName) =
-  MIL.TyTypeCon $ typeNameMil (getTypeName srcTypeName)
+  case getTypeName srcTypeName of
+    TypeName "IO" -> MIL.TyMonad $ MIL.MTyMonad MIL.IO
+    TypeName "State" -> error "srcTypeToMilType: State should have been handled earlier"
+    typeName -> MIL.TyTypeCon $ typeNameMil typeName
 srcTypeToMilType (SrcTyApp _ st1 st2) =
-  MIL.TyApp (srcTypeToMilType st1) (srcTypeToMilType st2)
+  case st1 of
+    SrcTyCon (getTypeName -> TypeName "State") ->
+      MIL.TyMonad $ MIL.MTyMonad (MIL.State $ srcTypeToMilType st2)
+    _ -> MIL.TyApp (srcTypeToMilType st1) (srcTypeToMilType st2)
 srcTypeToMilType (SrcTyArrow _ st1 st2) =
   MIL.TyArrow (srcTypeToMilType st1) (srcTypeToMilType st2)
 srcTypeToMilType (SrcTyForAll _ stv st) =
   MIL.TyForAll (typeVarMil $ getTypeVar stv) (srcTypeToMilType st)
 srcTypeToMilType (SrcTyParen _ st) = srcTypeToMilType st
 
--- Conversion utils
+-- * Conversion utils
 
 typeNameMil :: TypeName -> MIL.TypeName
 typeNameMil (TypeName typeNameStr) = MIL.TypeName typeNameStr
