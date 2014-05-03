40baad764f3dbcf1b247b6d8146d4a86e842ca7a
Dmytro Lypai <dmytrolypai@gmail.com>
2014-05-03 18:33:22 +0200

    OOLang. Code generator testing infrastructure.

---
+++ code/oolang/test/OOLang/CodeGenMilSpec.hs
@@ -0,0 +1,49 @@
+-- | MIL code generator tests.
+module OOLang.CodeGenMilSpec (main, spec) where
+
+import Test.Hspec
+import System.FilePath ((</>), (<.>))
+import Control.Monad (liftM2)
+import Control.Applicative ((<$>))
+
+import OOLang.Parser
+import OOLang.TypeChecker
+import OOLang.CodeGenMil
+import OOLang.TestUtils
+import qualified MIL.AST.PrettyPrinter as MIL
+
+-- | To be able to run this module from GHCi.
+main :: IO ()
+main = hspec spec
+
+-- Configuration
+
+testDir :: FilePath
+testDir = "test" </> "codegenmiltests"
+
+-- | Main specification function.
+spec :: Spec
+spec =
+  describe "codeGen" $ do
+    it "generates MIL code" $
+      testCase "Program"
+
+-- Infrastructure
+
+-- | Takes a file base name and performs a test (by comparing pretty printed
+-- code).
+testCase :: String -> IO ()
+testCase baseName = do
+  (input, output) <- testRead baseName
+  let Right srcProgram = parseOOLang (mkFileName baseName) input
+  let Right (tyProgram, _) = typeCheck srcProgram
+  let milProgram = codeGen tyProgram
+  (dropNewLine $ MIL.prPrint milProgram) `shouldBe` output
+
+-- | Takes a file base name and reads a source program and expected output
+-- (from .mil file).
+testRead :: String -> IO (String, String)
+testRead baseName =
+  liftM2 (,) (readFile (testDir </> mkFileName baseName))
+             (dropNewLine <$> readFile (testDir </> baseName <.> "mil"))
+
