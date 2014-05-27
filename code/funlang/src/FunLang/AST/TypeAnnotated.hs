c992957ef92fce49f8fa7a9e841e28fecb3f0838
Dmytro Lypai <dmytrolypai@gmail.com>
2014-05-27 15:25:01 +0200

    FunLang. Add TypeAnnotated instances.

--- code/funlang/src/FunLang/AST/TypeAnnotated.hs
+++ code/funlang/src/FunLang/AST/TypeAnnotated.hs
@@ -10,3 +10,38 @@ module FunLang.AST.TypeAnnotated
 import FunLang.AST
 import FunLang.TypeAnnotated
 
+instance TypeAnnotated Pattern where
+  getTypeOf (LitP lit) = getTypeOf lit
+  getTypeOf (VarP varBinder) = getTypeOf varBinder
+  getTypeOf (ConP _ t _ _) = t
+  getTypeOf (DefaultP _ t) = t
+  getTypeOf (ParenP _ pat) = getTypeOf pat
+
+instance TypeAnnotated Expr where
+  getTypeOf (LitE lit) = getTypeOf lit
+  getTypeOf (VarE _ t _) = t
+  getTypeOf (LambdaE _ t _ _) = t
+  getTypeOf (TypeLambdaE _ t _ _) = t
+  getTypeOf (TypeAppE _ t _ _) = t
+  getTypeOf (ConNameE t _) = t
+  getTypeOf (CaseE _ t _ _) = t
+  getTypeOf (LetE _ _ e) = getTypeOf e
+  getTypeOf (DoE _ t _) = t
+  getTypeOf (BinOpE _ t _ _ _) = t
+  getTypeOf (ThrowE _ t _) = t
+  getTypeOf (ParenE _ e) = getTypeOf e
+
+instance TypeAnnotated Literal where
+  getTypeOf (UnitLit _ t) = t
+  getTypeOf (IntLit _ t _) = t
+  getTypeOf (FloatLit _ t _ _) = t
+  getTypeOf (StringLit _ t _) = t
+
+-- | Bind statement should not be asked for type.
+instance TypeAnnotated Stmt where
+  getTypeOf (ExprS _ e) = getTypeOf e
+  getTypeOf (ReturnS _ t _) = t
+
+instance TypeAnnotated VarBinder where
+  getTypeOf (VarBinder _ t _ _) = t
+
