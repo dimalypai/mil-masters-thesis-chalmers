028fa82c02789d9d52db4b5b4c37f228163fc012
Dmytro Lypai <dmytrolypai@gmail.com>
2014-05-27 13:42:36 +0200

    FunLang. Fix comments and names in SrcAnnotated.

--- code/funlang/src/FunLang/SrcAnnotated.hs
+++ code/funlang/src/FunLang/SrcAnnotated.hs
@@ -3,14 +3,12 @@ module FunLang.SrcAnnotated where
 
 import FunLang.SrcSpan
 
--- | AST nodes with one type parameter (s) should be an instance of the
--- 'SrcAnnotated' type class.
 class SrcAnnotated ast where
   ann :: ast s -> s
 
 -- | Pair is an instance of 'SrcAnnotated'. Used for data types which are
 -- annotated as a pair.
-instance SrcAnnotated ((,) v) where
+instance SrcAnnotated ((,) a) where
   ann = snd
 
 -- | 'SrcSpan' specific synonym for 'ann'.
