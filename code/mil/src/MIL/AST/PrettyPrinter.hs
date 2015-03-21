9205013efa6f9e6c2509a9f78741cfbf9b3c5771
Dmytro Lypai <dmytrolypai@gmail.com>
2015-03-21 12:32:02 +0100

    MIL. Improve let indentation in the PrettyPrinter.

--- code/mil/src/MIL/AST/PrettyPrinter.hs
+++ code/mil/src/MIL/AST/PrettyPrinter.hs
@@ -57,8 +57,9 @@ instance (PrettyVar v, Pretty mt, Pretty t) => Pretty (Expr v ct mt t) where
     brackets (prPrn t)
   prPrn (ConNameE conName _) = prPrn conName
   prPrn (LetE varBind e1 e2) =
-    text "let" <+> prPrn varBind <+> text "<-" <+> prPrn e1 <+> text "in" $+$
-    nest indLvl (prPrn e2)
+    text "let" <+> prPrn varBind <+> text "<-" $+$
+      nest indLvl (prPrn e1 <+> text "in") $+$
+      nest (2 * indLvl) (prPrn e2)
   prPrn e@(ReturnE m e1) =
     text "return" <+> brackets (prPrn m) <+>
     prPrnParens (e1 `exprHasLowerPrec` e) e1
