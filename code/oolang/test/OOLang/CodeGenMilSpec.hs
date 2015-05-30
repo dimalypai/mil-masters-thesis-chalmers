736a40ce919a36baf7346c07afe8ec94ec6adddb
Dmytro Lypai <dmytrolypai@gmail.com>
2015-05-30 16:56:51 +0200

    OOLang. Improve debugging of failing tests in CodeGenMil.

--- code/oolang/test/OOLang/CodeGenMilSpec.hs
+++ code/oolang/test/OOLang/CodeGenMilSpec.hs
@@ -52,18 +52,20 @@ spec =
 testCase :: String -> IO ()
 testCase baseName = do
   (input, output) <- testRead baseName
-  let Right srcProgram = parseOOLang (mkFileName baseName) input
-  case typeCheck srcProgram of
-    Right (tyProgram, typeEnv) -> do
-      let srcMilProgram = MIL.parseMil output
-      case MIL.typeCheck srcMilProgram of
-        Right (tyMilProgram, _) -> do
-          let srcGenMilProgram = codeGen tyProgram typeEnv
-          case MIL.typeCheck srcGenMilProgram of
-            Right (tyGenMilProgram, _) -> MIL.prPrint tyGenMilProgram `shouldBe` MIL.prPrint tyMilProgram
+  case parseOOLang (mkFileName baseName) input of
+    Right srcProgram ->
+      case typeCheck srcProgram of
+        Right (tyProgram, typeEnv) -> do
+          let srcMilProgram = MIL.parseMil output
+          case MIL.typeCheck srcMilProgram of
+            Right (tyMilProgram, _) -> do
+              let srcGenMilProgram = codeGen tyProgram typeEnv
+              case MIL.typeCheck srcGenMilProgram of
+                Right (tyGenMilProgram, _) -> MIL.prPrint tyGenMilProgram `shouldBe` MIL.prPrint tyMilProgram
+                Left tcErr -> MIL.prPrint tcErr `shouldBe` ""
             Left tcErr -> MIL.prPrint tcErr `shouldBe` ""
-        Left tcErr -> MIL.prPrint tcErr `shouldBe` ""
-    Left tcErr -> prPrint tcErr `shouldBe` ""
+        Left tcErr -> prPrint tcErr `shouldBe` ""
+    Left parseErr -> prPrint parseErr `shouldBe` ""
 
 -- | Takes a file base name and reads a source program and expected output
 -- (from .mil file).
