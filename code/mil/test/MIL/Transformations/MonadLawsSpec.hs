433c36cc67f724c70a476fe695f45ea9138163e1
Dmytro Lypai <dmytrolypai@gmail.com>
2015-09-05 17:26:42 +0200

    MIL. Initial simplest tests for monad laws.

---
+++ code/mil/test/MIL/Transformations/MonadLawsSpec.hs
@@ -0,0 +1,58 @@
+-- | MIL monad laws transformations tests.
+module MIL.Transformations.MonadLawsSpec (main, spec) where
+
+import Test.Hspec
+import System.FilePath ((</>))
+import Control.Monad (liftM2)
+
+import MIL.AST
+import MIL.AST.PrettyPrinter
+import MIL.BuiltIn
+import MIL.Parser
+import MIL.TypeChecker
+import MIL.Transformations.MonadLaws
+import MIL.TestUtils
+
+-- | To be able to run this module from GHCi.
+main :: IO ()
+main = hspec spec
+
+-- | Configuration
+
+testDir :: FilePath
+testDir = "test" </> "transformationstests"
+
+-- | Main specification function.
+spec :: Spec
+spec = do
+  describe "leftIdentity" $ do
+    it "applies left identity monad law (simplest case)" $
+      testCase "LeftIdentity" leftIdentity
+
+  describe "rightIdentity" $ do
+    it "applies right identity monad law (simplest case)" $
+      testCase "RightIdentity" rightIdentity
+
+  describe "associativity" $ do
+    it "applies associativity monad law (simplest case)" $
+      testCase "Associativity" associativity
+
+testCase :: String -> (TyProgram -> TyProgram) -> IO ()
+testCase baseName trans = do
+  (input, output) <- testRead baseName
+  let srcInputProgram = parseMil input
+      srcOutputProgram = parseMil output
+  case ( typeCheck srcInputProgram (repeat defaultMonadError)
+       , typeCheck srcOutputProgram (repeat defaultMonadError)) of
+    (Right (tyInputProgram, _), Right (tyOutputProgram, _)) ->
+      prPrint (trans tyInputProgram) `shouldBe` prPrint tyOutputProgram
+    (Left inputTcError, Left outputTcError) ->
+      (prPrint inputTcError, prPrint outputTcError) `shouldBe` ("", "")
+    (Left inputTcError, _) -> prPrint inputTcError `shouldBe` ""
+    (_, Left outputTcError) -> prPrint outputTcError `shouldBe` ""
+
+testRead :: String -> IO (String, String)
+testRead baseName =
+  liftM2 (,) (readFile (testDir </> mkFileName baseName))
+             (readFile (testDir </> mkFileName (baseName ++ "_out")))
+
