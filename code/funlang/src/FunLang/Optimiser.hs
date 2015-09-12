a62b9312a02b87672f5caec2c90b7b82f4178c54
Dmytro Lypai <dmytrolypai@gmail.com>
2015-09-12 10:23:19 +0200

    OOLang and FunLang. Initial Optimiser tests setup.

--- code/funlang/src/FunLang/Optimiser.hs
+++ code/funlang/src/FunLang/Optimiser.hs
@@ -6,8 +6,9 @@ import qualified MIL.AST as MIL
 import qualified MIL.Transformations.MonadLaws as MILTrans
 
 optimiseMil :: MIL.TyProgram -> MIL.TyProgram
-optimiseMil milProgram = milProgram
-                      |> MILTrans.associativity
-                      |> MILTrans.leftIdentity
-                      |> MILTrans.rightIdentity
+optimiseMil milProgram =
+  milProgram
+  |> MILTrans.associativity
+  |> MILTrans.leftIdentity
+  |> MILTrans.rightIdentity
 
---
+++ code/oolang/test/OOLang/OptimiserSpec.hs
@@ -0,0 +1,59 @@
+-- | MIL optimiser tests.
+module OOLang.OptimiserSpec (main, spec) where
+
+import Test.Hspec
+import System.FilePath ((</>), (<.>))
+import Control.Monad (liftM2)
+
+import OOLang.Parser
+import OOLang.TypeChecker
+import OOLang.CodeGenMil
+import OOLang.Optimiser
+import OOLang.TestUtils
+
+import qualified MIL.AST.PrettyPrinter as MIL
+import qualified MIL.Parser as MIL
+import qualified MIL.TypeChecker as MIL
+
+-- | To be able to run this module from GHCi.
+main :: IO ()
+main = hspec spec
+
+-- Configuration
+
+testDir :: FilePath
+testDir = "test" </> "optimisertests"
+
+-- | Main specification function.
+spec :: Spec
+spec =
+  describe "optimiseMil" $ do
+    it "optimises the code (simple case)" $
+      testCase "Simple"
+
+-- | Takes a file base name and performs a test (by comparing pretty printed
+-- code).
+testCase :: String -> IO ()
+testCase baseName = do
+  (input, output) <- testRead baseName
+  let Right srcProgram = parseOOLang (mkFileName baseName) input
+  case typeCheck srcProgram of
+    Right (tyProgram, typeEnv) -> do
+      let srcGenMilProgram = codeGen tyProgram typeEnv
+      let srcMilProgram = MIL.parseMil output
+      case MIL.typeCheck srcMilProgram monadErrorTypeCons of
+        Right (tyMilProgram, _) -> do
+          case MIL.typeCheck srcGenMilProgram monadErrorTypeCons of
+            Right (tyGenMilProgram, _) ->
+              MIL.prPrint (optimiseMil tyGenMilProgram) `shouldBe` MIL.prPrint tyMilProgram
+            Left tcErr -> MIL.prPrint tcErr `shouldBe` ""
+        Left tcErr -> MIL.prPrint tcErr `shouldBe` ""
+    Left tcErr -> prPrint tcErr `shouldBe` ""
+
+-- | Takes a file base name and reads a source program and expected output
+-- (from .mil file).
+testRead :: String -> IO (String, String)
+testRead baseName =
+  liftM2 (,) (readFile (testDir </> mkFileName baseName))
+             (readFile (testDir </> (baseName ++ "_opt") <.> "mil"))
+
